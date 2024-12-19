9046dc8c63d5251fbfe9fd5ce7394152f44d1a8c
tilyupo <tilyupo@gmail.com>
2024-12-19 12:40:39 +0300

    add InMemoryLocker tests

--- packages/common/src/in-memory-locker.ts
+++ packages/common/src/in-memory-locker.ts
@@ -1,35 +1,46 @@
 import {Locker} from './contracts/locker';
 
 export class InMemoryLocker<TKey> implements Locker<TKey> {
-    private fnQueueMap: Map<any, Array<() => Promise<any>>> = new Map();
-    async lock<TResult>(key: TKey, fn: () => Promise<TResult>): Promise<TResult> {
-        const execute = async () => {
-            try {
-                return await fn();
-            } finally {
-                const blockedFn = (this.fnQueueMap.get(key) ?? []).pop();
-                if (blockedFn !== undefined) {
-                    blockedFn();
-                } else {
-                    this.fnQueueMap.delete(key);
-                }
-            }
-        };
+    private fnQueueMap: Map<TKey, Array<() => Promise<any>>> = new Map();
 
+    async lock<TResult>(key: TKey, fn: () => Promise<TResult>): Promise<TResult> {
+        return new Promise<TResult>((resolve, reject) => {
+            const execute = async () => {
+                try {
+                    const result = await fn();
+                    resolve(result);
+                } catch (error) {
+                    reject(error);
+                } finally {
+                    this.dequeue(key);
+                }
+            };
+
+            const fnQueue = this.fnQueueMap.get(key);
+
+            if (!fnQueue) {
+                this.fnQueueMap.set(key, []);
+                execute();
+            } else {
+                fnQueue.push(execute);
+            }
+        });
+    }
+
+    private dequeue(key: TKey): void {
         const fnQueue = this.fnQueueMap.get(key);
-        if (fnQueue === undefined) {
-            this.fnQueueMap.set(key, []);
-            return await execute();
-        } else {
-            return new Promise((resolve, reject) => {
-                fnQueue.push(async () => {
-                    try {
-                        resolve(await execute());
-                    } catch (err) {
-                        reject(err);
-                    }
-                });
-            });
+        if (!fnQueue) {
+            return;
+        }
+
+        if (fnQueue.length === 0) {
+            this.fnQueueMap.delete(key);
+            return;
+        }
+
+        const nextFn = fnQueue.shift();
+        if (nextFn) {
+            nextFn();
         }
     }
 }

6e50e42d3c6143a5c25e695c65e8e48386b2b777
tilyupo <tilyupo@gmail.com>
2024-12-23 12:05:48 +0300

    add yjs serializer

--- packages/common/src/collection.ts
+++ packages/common/src/collection.ts
@@ -1,18 +1,19 @@
+import {Doc} from 'yjs';
 import {KVStore} from './contracts/key-value-store';
 import {withKeySerializer, withPrefix, withValueSerializer} from './kv-store-utils';
-import {MsgpackrSerializer} from './msgpackr-serializer';
 import {pipe} from './utils';
 import {Uuid, UuidSerializer} from './uuid';
+import {YjsSerializer} from './yjs-serializer';
 
 export class Collection {
-    private documents: KVStore<Uuid, unknown>;
+    private documents: KVStore<Uuid, Doc>;
 
     constructor(kvStore: KVStore<Uint8Array, Uint8Array>) {
         this.documents = pipe(
             kvStore,
             withPrefix('d/'),
             withKeySerializer(new UuidSerializer()),
-            withValueSerializer(new MsgpackrSerializer())
+            withValueSerializer(new YjsSerializer())
         );
     }
 }

bcf33c55099a30856427d1a91d7328bbb4cde500
d3witt <d3witt@duck.com>
2025-03-18 23:12:28 +0100

    Fix

--- packages/app/src/pages/login-failed.svelte
+++ packages/app/src/pages/login-failed.svelte
@@ -1,10 +1,8 @@
-<div
-	class="flex h-screen w-screen flex-col items-center justify-center text-lg"
->
-	<h1 class="text-2xl font-bold">Login Failed</h1>
-	<p class="text-ink-detail mt-2 flex max-w-md items-center text-center">
-		Your login attempt was unsuccessful. Please check your credentials and try
-		again.
+<div class="flex h-screen w-screen flex-col items-center">
+	<h1 class="text-2xl font-extrabold mt-[30vh]">Login Failed</h1>
+	<p class="text-ink-detail mt-1 flex max-w-sm items-center text-center">
+		The login attempt was unsuccessful. Please verify your email, or try signing
+		in with Google.
 	</p>
-	<a href="/login" class="mt-4">Try Again</a>
+	<a href="/login" class="mt-4">Go back to login page</a>
 </div>

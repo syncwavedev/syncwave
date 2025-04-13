725202fc2289dc388e9523fea41a07af70b24683
d3witt <d3witt@duck.com>
2025-04-13 19:53:01 +0200

    feat: create board

--- packages/app/src/lib/ui/boards/add-board-modal.svelte
+++ packages/app/src/lib/ui/boards/add-board-modal.svelte
@@ -1,15 +1,35 @@
 <script lang="ts">
+    import {getAgent} from '../../agent/agent';
+    import router from '../../router';
     import CommandHeader from '../command-center/command-header.svelte';
     import CommandView from '../command-center/command-view.svelte';
     import PlusIcon from '../components/icons/plus-icon.svelte';
+    import modalManager from '../modal-manager.svelte';
 
-    let filter = $state('');
+    let name = $state('');
+
+    const agent = getAgent();
+
+    async function onCreateBoard() {
+        if (name.length < 3) {
+            return;
+        }
+
+        await agent.createBoard({key: name, name: name, memberEmails: []});
+        modalManager.close();
+        router.route(`/b/${name}`);
+    }
 </script>
 
 <CommandView>
-    <CommandHeader placeholder="Enter board name..." bind:filter />
+    <CommandHeader placeholder="Enter board name..." bind:filter={name} />
     <div class="modal-footer mx-2">
-        <button type="submit" class="btn-ghost ml-auto">
+        <button
+            type="submit"
+            class="btn-ghost ml-auto"
+            onclick={onCreateBoard}
+            disabled={name.length < 3}
+        >
             <PlusIcon />
             Create
         </button>

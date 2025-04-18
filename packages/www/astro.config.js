1c6a434650ae018d3ea4e4c7a95dac53e881bfcd
tilyupo <tilyupo@gmail.com>
2025-04-18 09:33:46 +0300

    chore: add www tailwind support

--- packages/www/astro.config.js
+++ packages/www/astro.config.js
@@ -1,4 +1,9 @@
+import tailwindcss from '@tailwindcss/vite';
 import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});

0c30b73e13a5a5acdc6912c74b9c204ff884a81c
d3witt <d3witt@duck.com>
2025-04-19 13:29:44 +0200

    fix: design

--- packages/app/src/lib/ui/boards/message-list.svelte
+++ packages/app/src/lib/ui/boards/message-list.svelte
@@ -13,7 +13,7 @@
 <div class="flex flex-col gap-4 flex-1 relative">
     {#each messages as message, index (message.id)}
         <div class="flex flex-col panel-padding-inline relative">
-            <div class="flex items-center gap-1.5 relative">
+            <div class="flex items-center gap-1.5 relative avatar-sm">
                 <Avatar name={message.author.fullName} />
 
                 <div class="flex items-baseline gap-1.5">

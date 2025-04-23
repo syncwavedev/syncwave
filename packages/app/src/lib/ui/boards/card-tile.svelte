e84cab501ca48d2468bd54d0f1b0d873801557d9
d3witt <d3witt@duck.com>
2025-04-23 22:45:52 +0200

    design: iterate

--- packages/app/src/lib/ui/boards/card-tile.svelte
+++ packages/app/src/lib/ui/boards/card-tile.svelte
@@ -127,7 +127,7 @@
                 data-active={active || undefined}
             >
                 {#if !card.isDraft}
-                    <span class="truncate">
+                    <span class="truncate font-medium">
                         {preview}
                     </span>
                 {/if}

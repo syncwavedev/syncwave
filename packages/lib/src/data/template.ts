a9d80b2257d838f0cb43ac3dbded3d4612c6bc56
tilyupo <tilyupo@gmail.com>
2025-04-23 19:37:22 +0300

    chore: add new board template

--- packages/lib/src/data/template.ts
+++ packages/lib/src/data/template.ts
@@ -82,6 +82,72 @@ export const BOARD_ONBOARDING_TEMPLATE: BoardTemplate = {
     ],
 };
 
+export const NEW_BOARD_TEMPLATE: BoardTemplate = {
+    columns: [
+        {
+            name: 'Backlog',
+            cards: [
+                {
+                    html: `
+                        <p>Welcome to Syncwave!</p>
+                        <ul>
+                            <li><b>Syncwave</b> is a collaborative tool for managing your projects.</li>
+                            <li>Use it to track tasks, share ideas, and collaborate with your team.</li>
+                            <li>Get started by creating a new board or joining an existing one.</li>
+                            <li>Invite your team members to collaborate with you.</li>
+                        </ul>
+                        <p>Click on the <i>"Create Board"</i> button to get started.</p>
+                    `,
+                    messages: [
+                        {
+                            html: '<p>We are glad to have you here.</p>',
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            name: 'To Do',
+            cards: [
+                {
+                    html: "<p>Let's get started!</p>",
+                    messages: [
+                        {
+                            html: '<p>Here are some tips to get you started.</p>',
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            name: 'In Progress',
+            cards: [
+                {
+                    html: '<p>Keep going!</p>',
+                    messages: [
+                        {
+                            html: '<p>You are doing great!</p>',
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            name: 'Done',
+            cards: [
+                {
+                    html: '<p>Congratulations!</p>',
+                    messages: [
+                        {
+                            html: '<p>You have completed the onboarding process.</p>',
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+};
+
 export const BOARD_DEMO_TEMPLATE: BoardTemplate = {
     columns: [
         {

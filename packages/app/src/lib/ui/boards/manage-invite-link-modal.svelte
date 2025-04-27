91f2a76857c12812e3c7439e029bf2c3316325fd
d3witt <d3witt@duck.com>
2025-04-27 15:11:14 +0200

    chore: use agent for invite link actions

--- packages/app/src/lib/ui/boards/manage-invite-link-modal.svelte
+++ packages/app/src/lib/ui/boards/manage-invite-link-modal.svelte
@@ -4,10 +4,25 @@
     import Modal from '../components/modal.svelte';
     import RefreshIcon from '../components/icons/refresh-icon.svelte';
     import {appConfig} from '../../config';
+    import {getAgent} from '../../agent/agent.svelte';
 
     let {board}: {board: BoardView} = $props();
 
+    const agent = getAgent();
+
     const joinLink = $derived(`${appConfig.uiUrl}join/${board.joinCode}`);
+
+    let isUpdating = $state(false);
+    async function onRefreshClick() {
+        isUpdating = true;
+        await agent.updateJoinCode(board.id).finally(() => {
+            isUpdating = false;
+        });
+    }
+
+    async function onCopyClick() {
+        await navigator.clipboard.writeText(joinLink);
+    }
 </script>
 
 <Modal title="Manage Invite Link" size="md">
@@ -22,10 +37,17 @@
         />
 
         <div class="flex gap-4 icon-lg">
-            <button class="btn--icon btn--icon--bordered btn--icon--lg">
+            <button
+                class="btn--icon btn--icon--bordered btn--icon--lg"
+                onclick={onRefreshClick}
+                disabled={isUpdating}
+            >
                 <RefreshIcon />
             </button>
-            <button class="btn--icon btn--icon--bordered btn--icon--lg">
+            <button
+                class="btn--icon btn--icon--bordered btn--icon--lg"
+                onclick={onCopyClick}
+            >
                 <ClipboardCopyIcon />
             </button>
         </div>

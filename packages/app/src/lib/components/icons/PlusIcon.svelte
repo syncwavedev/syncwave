f4b502df9c7d521c4476b0ac49f57bb86be898fb
tilyupo <tilyupo@gmail.com>
2025-08-07 11:59:43 +0300

    fix: make adding new card work

--- packages/app/src/lib/components/icons/PlusIcon.svelte
+++ packages/app/src/lib/components/icons/PlusIcon.svelte
@@ -1,3 +1,3 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon pointer-events-none">
     <path d="M5 12h14" /><path d="M12 5v14" />
 </svg>
